edb942020e5f3cd8ea1893464f767ccae81aa496
VivekGujale <vdgujale007@.com>
2023-01-03 16:11:05 +0530

    Set Event listner when document is loaded

--- new_payroll_form.js
+++ new_payroll_form.js
@@ -1,14 +1,21 @@
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function () {
-    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if (nameRegex.test(text.value))
-        textError.textContent = "";
-    else textError.textContent = "Name is Incorrect";
-});
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const text = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    text.addEventListener('input', function () {
+        if (text.value.length == 0) {
+            textError.textContent = "";
+            return;
+        } try {
+            (new EmployeePayrollData()).name = text.value;;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
 });
